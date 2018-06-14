ca0b70625ba233428107a5b0ef121fd6a17c10de
setayesh92 <36989055+setayesh92@users.noreply.github.com>
2018-06-14 11:56:59 +0430

    Add project files.

---
+++ ShopOnlineMvc1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopOnlineMvc1.Startup))]
+namespace ShopOnlineMvc1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
